0534252fc446618f67f0b8935c044a45a1681402
karloid <krldboyan@gmail.com>
2015-04-25 15:18:51 +0300

    Initial commit

---
+++ src/main/java/ChatServerLauncher.java
@@ -0,0 +1,9 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+
+public class ChatServerLauncher {
+	public static void main(String[] args) {
+		System.out.println("Lol");
+		Server server = new Server();
+	}
+}
