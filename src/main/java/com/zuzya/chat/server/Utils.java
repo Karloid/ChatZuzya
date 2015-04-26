ae09124772ebe27383cff22cbfe559a8269d20f4
karloid <krldboyan@gmail.com>
2015-04-26 23:21:46 +0300

    save external ip to git repo :3

--- src/main/java/com/zuzya/chat/server/Utils.java
+++ src/main/java/com/zuzya/chat/server/Utils.java
@@ -1,15 +1,46 @@
 package com.zuzya.chat.server;
 
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 
 public class Utils {
 
 	public static void setupIp() {
 		String ipAddress = getIpAddress();
+		resetLastGitCommit(ipAddress);
+	}
+
+	private static void resetLastGitCommit(String ipAddress) {
+
+		try {
+			File workingDir = new File("../SomeInfo");
+
+			runCmdCommand(workingDir, "cmd.exe /c start /wait cmd.exe /k git pull -v");
+
+			runCmdCommand(workingDir, "cmd.exe /c start /wait cmd.exe /k git reset HEAD~1");
+
+			runCmdCommand(workingDir, "cmd.exe /c start /wait cmd.exe /k git push origin +HEAD");
+
+			writeToFile(ipAddress);
+
+			runCmdCommand(workingDir, "cmd.exe /c start /wait cmd.exe /k git add .");
+
+			runCmdCommand(workingDir, "cmd.exe /c start /wait cmd.exe /k git commit -m \"new\"");
+
+			runCmdCommand(workingDir, "cmd.exe /c start /wait cmd.exe /k git push origin master");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void runCmdCommand(File workingDir, String cmd) throws IOException, InterruptedException {
+		Process p;
+		p = Runtime.getRuntime().exec(cmd, null, workingDir);
+		p.waitFor();
+	}
+
+	private static void writeToFile(String ipAddress) {
 		try (FileOutputStream outputStream = new FileOutputStream("../SomeInfo/doc", false)) {//TODO normal path :>
 			outputStream.write(ipAddress.getBytes());
 			outputStream.flush();
